65eeb643ff96b088e9c99d17bdcfdf7f64b4fb0c
Jay Jain <jay4codes@gmail.com>
2022-01-30 09:19:17 +0530

    js files & style.css added

---
+++ static/js/index.js
@@ -0,0 +1,25 @@
+const log = console.log;
+
+const chartProperties = {
+  width:1500,
+  height:600,
+  timeScale:{
+    timeVisible:true,
+    secondsVisible:false,
+  }
+}
+
+const domElement = document.getElementById('tvchart');
+const chart = LightweightCharts.createChart(domElement,chartProperties);
+const candleSeries = chart.addCandlestickSeries();
+
+
+fetch(`https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000`)
+  .then(res => res.json())
+  .then(data => {
+    const cdata = data.map(d => {
+      return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
+    });
+    candleSeries.setData(cdata);
+  })
+  .catch(err => log(err))
